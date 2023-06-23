f5c5c4ee2b6a93e557556693b09ee9f8e8a10d6f
robertkleinschuster <robert@kleinschuster.de>
2023-06-24 01:01:49 +0200

    fix include paths

--- services/example/public/index.php
+++ services/example/public/index.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-chdir(dirname(__DIR__));
-
 require '../../../vendor/autoload.php';
+
+chdir(dirname(__DIR__));
 
 $api = Blue\Snappy\Core\Http::htmlApp();
 $api->onGET('index', '/', new Blue\TrainsearchApi\TripHandler());
--- services/trainsearch/api/public/index.php
+++ services/trainsearch/api/public/index.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-chdir(dirname(__DIR__));
-
 require '../../../../vendor/autoload.php';
 
+chdir(dirname(__DIR__));
+
 $api = Blue\Snappy\Core\Http::jsonApi();
-$api->onGET('trip', '/api/{profile}/trip/{query}', new Blue\TrainsearchApi\TripHandler());
-$api->onGET('composition', '/api/oebb/composition/{query}', new Blue\TrainsearchApi\CompositionHandler());
+$api->onGET('trip', '/{profile}/trip/{query}', new Blue\TrainsearchApi\TripHandler());
+$api->onGET('composition', '/oebb/composition/{query}', new Blue\TrainsearchApi\CompositionHandler());
 
 $api->run();
