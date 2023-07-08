a4100cd1130dbf53900a444df18d3737e937bf27
robertkleinschuster <github@kleinschuster.de>
2023-07-08 10:00:04 +0200

    trainsearch remove result on error

--- apps/trainsearch/frontend/src/Client/TrainSearchClient.ts
+++ apps/trainsearch/frontend/src/Client/TrainSearchClient.ts
@@ -4,16 +4,25 @@ export default class TrainSearchClient {
 
     public async trip(nr: string, operator: string, source: string) {
         const response = await fetch(`${this.endpoint}/${source}/trip/${nr}?operator=${operator}`)
+        if (!response.ok) {
+            return null;
+        }
         return await response.json()
     }
 
     public async operators(source: string) {
         const response = await fetch(`${this.endpoint}/${source}/operators`)
+        if (!response.ok) {
+            return null;
+        }
         return await response.json()
     }
 
     public async compostion(nr: string, stationId: string, source: string) {
         const response = await fetch(`${this.endpoint}/${source}/composition/${nr}?station=${stationId}`)
+        if (!response.ok) {
+            return null;
+        }
         return await response.json();
     }
 }
