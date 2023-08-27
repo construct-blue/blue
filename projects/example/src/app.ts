90cba7a8f518dae831daab143da53d6273be32df
robertkleinschuster <git@robs.social>
2023-08-27 15:50:55 +0200

    custom elements autocomplete

--- projects/example/src/app.ts
+++ projects/example/src/app.ts
@@ -4,6 +4,6 @@ import {customElement} from "lit/decorators.js"
 @customElement('example-app')
 class ExampleApp extends LitElement {
     protected render(): unknown {
-        return html`<h1>Hello world!</h1><h2>This is a test</h2>`;
+        return html`<h1>Hello world!</h1>`;
     }
 }
